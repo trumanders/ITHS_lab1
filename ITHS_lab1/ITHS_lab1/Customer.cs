0946026e6b3c85880ea0386e21af17ff14190b34
trumanders <trumanders@gmail.com>
2022-09-09 14:44:56 +0200

    Added three classes

---
+++ ITHS_lab1/ITHS_lab1/Customer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITHS_lab1
+{
+    internal class Customer
+    {
+    }
+}
