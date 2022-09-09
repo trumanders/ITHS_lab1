baf6a6a44164533c955fac77e9b4e2b767623fea
trumanders <trumanders@gmail.com>
2022-09-09 15:28:17 +0200

    Converted Account-class

--- ITHS_lab1/ITHS_lab1/Account.cs
+++ ITHS_lab1/ITHS_lab1/Account.cs
@@ -6,98 +6,101 @@ using System.Threading.Tasks;
 
 namespace ITHS_lab1
 {
-    package andjox1;
-
-    import java.math.BigDecimal;
-
-    /**
-     * This class defines the bank accounts.
-     * @author Anders Johansson, andjox-1
-     */
-    public class Account
+    class Account
     {
-        private static int countingAllAccountNumbers = 1000;
-        private static final BigDecimal INTEREST_RATE = new BigDecimal(1.2);
-        private static final String ACCOUNT_TYPE = "Sparkonto";
-    private int accountNumber;
-        private BigDecimal balance = new BigDecimal(0);
-
-
-        /* Constructor */
-        public Account()
-        {
-            countingAllAccountNumbers++;
-            accountNumber = countingAllAccountNumbers;
-        }
-
-
-        /* TRANSACTIONS */
-
         /**
-         * Calculates the new balance after a deposit
-         * @param amount    The amount of money to deposit
+         * This class defines the bank accounts.
+         * @author Anders Johansson, anders.johansson@iths.se
          */
-        public void deposit(int amount)
+        public class Account
         {
-            balance = balance.add(new BigDecimal(amount));
-        }
-
-        /**
-         * Calculates the balance after a withdrawal
-         * @param amount    The amount of money to withdraw
-         */
-        public void withdraw(int amount)
-        {
-            balance = balance.subtract(new BigDecimal(amount));
-        }
-
-        /* GETTERS */
-
-        /**
-         * Gets the account number
-         * @return int  The account number
-         */
-        public int getAccountNumber()
-        {
-            return this.accountNumber;
-        }
-
-        /**
-         * Gets the balance od the account
-         * @return BigDecimal   The account balance
-         */
-        public BigDecimal getBalance()
-        {
-            return balance;
-        }
-
-        /**
-         * Gets the account type
-         * @return String   The account type
-         */
-        public static String getACCOUNT_TYPE()
-        {
-            return ACCOUNT_TYPE;
-        }
-
-        /**
-         * Gets the interest rate of all accounts (static)
-         * @return BigDecimal   The interest that all accounts have
-         */
-        public static BigDecimal getInterestRate()
-        {
-            return INTEREST_RATE;
-        }
+            private static int countingAllAccountNumbers = 1000;
+            private const Decimal INTEREST_RATE = 1.2m;
+            private const String ACCOUNT_TYPE = "Sparkonto";
+            private int accountNumber;
+            private Decimal balance = 0;
 
 
-        /**
-         * Gets the account number of the last added account
-         * @return int  The account number of the last added account
-         */
-        public static int getCountingAllAccountNumbers()
-        {
-            return countingAllAccountNumbers;
+            /* Constructor */
+            public Account()
+            {
+                countingAllAccountNumbers++;
+                accountNumber = countingAllAccountNumbers;
+            }
+
+
+            /* TRANSACTIONS */
+
+            /**
+             * Calculates the new balance after a deposit
+             * @param amount    The amount of money to deposit
+             */
+            public void deposit(int amount)
+            {
+                balance += amount;
+            }
+
+
+            /**
+             * Calculates the balance after a withdrawal
+             * @param amount    The amount of money to withdraw
+             */
+            public void withdraw(int amount)
+            {
+                balance -= amount;
+            }
+
+
+            /* GETTERS */
+
+            /**
+             * Gets the account number
+             * @return int  The account number
+             */
+            public int getAccountNumber()
+            {
+                return this.accountNumber;
+            }
+
+
+            /**
+             * Gets the balance od the account
+             * @return Decimal   The account balance
+             */
+            public Decimal getBalance()
+            {
+                return balance;
+            }
+
+
+            /**
+             * Gets the account type
+             * @return String   The account type
+             */
+            public static String getACCOUNT_TYPE()
+            {
+                return ACCOUNT_TYPE;
+            }
+
+
+            /**
+             * Gets the interest rate of all accounts (static)
+             * @return Decimal   The interest that all accounts have
+             */
+            public static Decimal getInterestRate()
+            {
+                return INTEREST_RATE;
+            }
+
+
+            /**
+             * Gets the account number of the last added account
+             * @return int  The account number of the last added account
+             */
+            public static int getCountingAllAccountNumbers()
+            {
+                return countingAllAccountNumbers;
+            }
         }
     }
-
 }
